497784eefdf00fe88812343b752d3750f8cb948f
Harsh-Agrawal96 <harshagrawal9650@gmail.com>
2024-01-22 22:46:11 +0530

    build the code for showing lapse at different intervals

--- Stopwatch/Stopwatch.js
+++ Stopwatch/Stopwatch.js
@@ -49,3 +49,130 @@ function restartclock(){
 start.addEventListener('click',startclock);
 sto.addEventListener('click',stopclock);
 reset.addEventListener('click',restartclock);
+
+
+// below code is for the showing lapse on click flag type button ( which will we add soon )
+
+var clickcount = 0;
+var newlapseleft;
+var newlapsemid;
+var newlapseright;
+
+const lapse = document.querySelector('button');
+
+lapse.addEventListener('click',function() {
+    console.log("je");
+    if( clickcount == 0){
+        clickcount = 1;
+        create_first_lapse();
+    }else{
+        clickcount++;
+        add_new_lapse();
+    }
+})
+
+function add_new_lapse(){
+
+    const leftinitial = document.createElement('div');// make divs for lapse
+    const midinitial = document.createElement('div');
+    const rightinitial = document.createElement('div');
+
+    const leftdata = document.createElement('p');// make paragraph for lapse
+    leftdata.textContent = clickcount;
+    const middata = document.createElement('p');
+    middata.textContent = "hello mid";
+    const rightdata = document.createElement('p');
+    rightdata.textContent = "hello right";
+
+    leftinitial.appendChild(leftdata); // attach para to div 
+    midinitial.appendChild(middata);
+    rightinitial.appendChild(rightdata);
+
+    newlapseleft.appendChild(leftinitial);// attach div to main div which content all lapse
+    newlapsemid.appendChild(midinitial);
+    newlapseright.appendChild(rightinitial);
+}
+
+function create_first_lapse(){
+    const main = document.createElement('div');
+
+    const left = document.createElement('div');
+    const mid = document.createElement('div');
+    const right = document.createElement('div');
+
+    const lefthead = document.createElement('div');
+    const midhead = document.createElement('div');
+    const righthead = document.createElement('div');
+
+    const leftheadcontent = document.createElement('p'); // make paragraph for head contents
+    leftheadcontent.textContent = "Laps";
+    const midheadcontent = document.createElement('p');
+    midheadcontent.textContent = "Time";
+    const rightheadcontent = document.createElement('p');
+    rightheadcontent.textContent = "Total";
+
+    lefthead.appendChild(leftheadcontent);
+    midhead.appendChild(midheadcontent);
+    righthead.appendChild(rightheadcontent);
+
+    newlapseleft = document.createElement('div');
+    newlapsemid = document.createElement('div');
+    newlapseright = document.createElement('div');
+
+
+//  code for first lapse
+
+    const leftinitial = document.createElement('div');// make first divs for lapse
+    const midinitial = document.createElement('div');
+    const rightinitial = document.createElement('div');
+
+    const leftdata = document.createElement('p'); // make paragraph for first lapse
+    leftdata.textContent = clickcount;
+    const middata = document.createElement('p');
+    middata.textContent = "hello mid";
+    const rightdata = document.createElement('p');
+    rightdata.textContent = "hello right";
+
+    leftinitial.appendChild(leftdata); // attach para to div
+    midinitial.appendChild(middata);
+    rightinitial.appendChild(rightdata);
+
+// maked divs for first lapse
+
+    newlapseleft.appendChild(leftinitial);  // attach div to main div which content all lapse
+    newlapsemid.appendChild(midinitial);
+    newlapseright.appendChild(rightinitial);
+
+    left.appendChild(lefthead);
+    left.appendChild(newlapseleft);
+
+    mid.appendChild(midhead);
+    mid.appendChild(newlapsemid);
+
+    right.appendChild(righthead);
+    right.appendChild(newlapseright);
+
+    main.appendChild(left);
+    main.appendChild(mid);
+    main.appendChild(right);
+
+//  adding styles for this 
+    main.classList.add('base');
+
+    left.classList.add('inner1');
+    mid.classList.add('inner1');
+    right.classList.add('inner1');
+
+    lefthead.classList.add('head');
+    midhead.classList.add('head');
+    righthead.classList.add('head');
+
+    newlapseleft.classList.add('content');
+    newlapsemid.classList.add('content');
+    newlapseright.classList.add('content');
+// added all css
+
+// now add it to main body
+    document.body.appendChild(main);
+
+}
